e84144f6bf2a3f4427a39fec24fd01177a15983d
cyq7on <cyq7on@gmail.com>
2018-05-05 13:12:33 +0800

    remove excel and add table

---
+++ app/src/main/java/cn/bmob/imdemo/adapter/RecordAdapter.java
@@ -0,0 +1,56 @@
+package cn.bmob.imdemo.adapter;
+
+import android.widget.TextView;
+
+import com.app.feng.fixtablelayout.inter.IDataAdapter;
+
+import java.util.List;
+
+import cn.bmob.imdemo.bean.Record;
+
+public class RecordAdapter implements IDataAdapter {
+
+    private String[] titles;
+    private List<Record> data;
+
+    public RecordAdapter(String[] titles,List<Record> data) {
+        this.titles = titles;
+        this.data = data;
+    }
+
+    public void setData(List<Record> data) {
+        this.data = data;
+    }
+
+
+    @Override
+    public String getTitleAt(int pos) {
+        return titles[pos];
+    }
+
+    @Override
+    public int getTitleCount() {
+        return titles.length;
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    @Override
+    public void convertData(int i, List<TextView> list) {
+        Record record = data.get(i);
+        list.get(0).setText(record.name);
+        list.get(1).setText(record.idCard);
+        list.get(2).setText(record.plateNum);
+        list.get(0).setText(record.score);
+        list.get(0).setText(record.type);
+        list.get(0).setText(record.tel);
+    }
+
+    @Override
+    public void convertLeftData(int i, TextView textView) {
+        textView.setText(data.get(i).getObjectId());
+    }
+}
